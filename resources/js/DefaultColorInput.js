ecf7cfbd3471ccd21433d8e230fcf0e4fd3995b6
Roel van Hintum <roel@born05.com>
2018-03-01 10:23:38 +0100

    Js validation fixes.

--- resources/js/DefaultColorInput.js
+++ resources/js/DefaultColorInput.js
@@ -10,21 +10,21 @@
                 // You would be wrong.
                 // ids are duplicated in live preview
                 var $field = $me.closest('#'+$me.data('colorid'));
-                var $input = $colorField.find('input[type="hidden"]');
-                var $colorInput = $colorField.find('input[type="color"]');
+                var $input = $field.find('input[type="hidden"]');
+                var $colorInput = $field.find('input[type="color"]');
                 $input.val(defaultColor);
                 $colorInput.val(defaultColor);
                 return false;
             });
-            
+
             $(document).on('change', 'input[type="color"]', function (e) {
                 var $colorInput = $(e.currentTarget);
 
                 // you would think that since this is an id it is unique, and we don't need the closest().
                 // You would be wrong.
                 // ids are duplicated in live preview
-                var $input = $colorField.siblings('input[type="hidden"]');
-                $input.val(defaultColor);
+                var $input = $colorInput.siblings('input[type="hidden"]');
+                $input.val($colorInput.val());
                 return false;
             });
         });
